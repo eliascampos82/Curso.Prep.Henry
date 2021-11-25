ead7ca96fefa09560f53352063c7320dae2505d2
unknown <fcutufia@hotmail.com>
2021-11-25 13:12:57 -0300

    primer cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "elias";
 const nuevoNum = 15;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;

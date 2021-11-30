416ad69901ecc85cc0fb67bc6221c2c44a0473e1
github_eliascampos82 <eliascampos2949@gmail.com>
2021-11-29 21:43:48 -0300

    Agregado nuevo archivo, 02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "elias";
 const nuevoNum = 56;
 
 // Crea una variable booleana:
-const nuevoBool = false;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
